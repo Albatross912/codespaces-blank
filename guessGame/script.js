dba556d28549b63d3ea0131e4bda5186b5a7e359
Albatross912 <piyushsharma912912@gmail.com>
2022-12-07 22:43:20 +0530

    d

---
+++ guessGame/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    document.getElementById("number-submit").addEventListener("click", playgame);
+    document.getElementById("restart-game").addEventListener("click", initGame);
+}
+function playgame()
+{ 
+    
+}
+function initGame()
+{
+
+}
